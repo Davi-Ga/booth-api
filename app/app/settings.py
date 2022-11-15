cafd766e7d1d157bb19fe15cd210faad2fc496cb
Davi-Ga <davigaldinoky@gmail.com>
2022-11-15 00:59:31 -0300

    debug ajustado para .env

--- app/app/settings.py
+++ app/app/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('DEBUG'))
 
 ALLOWED_HOSTS = []
 
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'booth.apps.BoothConfig'
+    'user.apps.UserConfig'
 ]
 
 MIDDLEWARE = [
