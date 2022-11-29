c6c11cbe917fd065c29cfa1ca7b1f6f3493c976d
Davi-Ga <davigaldinoky@gmail.com>
2022-11-29 11:55:31 -0300

    form register alterado

--- src/booth/forms.py
+++ src/booth/forms.py
@@ -1,9 +1,11 @@
 from django.forms import ModelForm
 from .models import Booth
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-class RegisterForm(forms.ModelForm):
+class RegisterForm(UserCreationForm):
     class Meta:
-        model = Booth
+        model = User
         fields = ['username','email','password']
         
