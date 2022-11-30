34ec21bdd58d97a7c6b6dbd5fcfcb0d8a42f6ac4
Davi-Ga <davigaldinoky@gmail.com>
2022-11-29 22:02:01 -0300

    form booth criado

--- src/booth/forms.py
+++ src/booth/forms.py
@@ -1,11 +1,9 @@
 from django.forms import ModelForm
 from .models import Booth
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 
-class RegisterForm(UserCreationForm):
+class RegisterForm(forms.ModelForm):
     class Meta:
-        model = User
-        fields = ['username','email','password']
+        model = Booth
+        fields = ['title','description']
         
