fdc50c21fe1106b111acbc26279c3528fd62e227
Davi-Ga <davigaldinoky@gmail.com>
2022-12-11 18:34:03 -0300

    BoothCreateForm

--- src/booth/forms.py
+++ src/booth/forms.py
@@ -2,7 +2,7 @@ from django.forms import ModelForm
 from .models import Booth
 from django import forms
 
-class RegisterForm(forms.ModelForm):
+class BoothCreateForm(forms.ModelForm):
     class Meta:
         model = Booth
         fields = ['title','description']
