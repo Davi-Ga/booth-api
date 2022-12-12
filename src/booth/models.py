27c7687e17cc5ecb08b2cf7ee82e5e7ee10f70f8
Davi-Ga <davigaldinoky@gmail.com>
2022-12-12 10:10:27 -0300

    models booth

--- src/booth/models.py
+++ src/booth/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Booth(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Photo(models.Model):
+    title=models.CharField(max_length=50)
+    booth=models.ForeignKey(Booth, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.id
