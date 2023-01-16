8f988c1f8c29753b738527a17e6f2d51a6d1c37c
Davi-Ga <davigaldinoky@gmail.com>
2023-01-16 18:37:59 -0300

    models inclued

--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    nome=models.CharField(max_length=50,blank=False,null=False)
+    email=models.EmailField(blank=False,null=False)
+    senha=models.CharField(max_length=50,blank=False,null=False)
+    
+    def __str__(self):
+        return self.nome
+    
