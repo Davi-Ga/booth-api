5f2516d0faeb5f2e4c808b95997237a1f095bf27
Davi-Ga <davigaldinoky@gmail.com>
2023-01-30 12:21:43 -0300

    new validators

--- src/user/validators.py
+++ src/user/validators.py
@@ -1,3 +1,10 @@
+from django.core.exceptions import ValidationError
+import re
+
 def validation_email(email):
     if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
         raise ValidationError('Invalid email address')
+
+def validation_password(password):
+    if not re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$", password):
+        raise ValidationError('Invalid password')
