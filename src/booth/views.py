d077fb5c9a4a6a8fb75094674ddb7b7e7ccc54c6
Davi-Ga <davigaldinoky@gmail.com>
2023-02-20 00:27:27 -0300

    home

--- src/booth/views.py
+++ src/booth/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import request
-from models import Booth
+from .models import Booth
 
 def home(request):
-    Booth.object.all()
+
 
     context={
 
     }
-    return render(request,'/home.html',context=context)
+    return render(request,'home.html',context=context)
 
 def add_booth(request):
     context={
