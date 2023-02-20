a871f3ff1a4f06f3f32cdd7fbe8351c67f552760
Davi-Ga <davigaldinoky@gmail.com>
2023-02-20 15:06:46 -0300

    forms

--- src/user/views.py
+++ src/user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from forms import RegisterForm
+from .forms import RegisterForm
 from django.views.decorators.cache import cache_page
 
 #ajustar esse registro
