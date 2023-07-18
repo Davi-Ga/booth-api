c98b9f3e1bc99bfbb02c99f85506160d4ed8cc27
Davi-Ga <davigaldinoky@gmail.com>
2023-07-18 03:18:00 -0300

    fix: rm arquivos desnecessários

--- src/booth/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import Booth
-from django import forms
-
-class BoothCreateForm(forms.ModelForm):
-    class Meta:
-        model = Booth
-        fields = 'all'
-        
--- src/user/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-
-class RegisterForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username','email','password']
-        
