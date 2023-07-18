5538b13f2267aa752726f9624c92ce7d29588bc5
Davi-Ga <davigaldinoky@gmail.com>
2023-07-18 03:18:26 -0300

    feat: api view criada

--- src/booth/views.py
+++ src/booth/views.py
@@ -1,17 +1,16 @@
-from django.shortcuts import render
-from django.http import request
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
+from rest_framework.views import Response
 from .models import Booth
+from .serializers import BoothSerializer
 
-def home(request):
 
-
-    context={
-
-    }
-    return render(request,'home.html',context=context)
-
-def add_booth(request):
-    context={
-
-    }
-    return render(request,'/add_booth.html',context=context)
+class BoothAPIView(APIView):
+    def get(self,format=None):
+        queryset=Booth.objects.all()
+        serializer=BoothSerializer(queryset,many=True)
+        
+        return Response(serializer.data,status=HTTP_200_OK)
+   
+    
+    
